9f887e0cf1b06f76f4b7e646cc1e6b5f010388a5
Ben Lee <bendelonlee@gmail.com>
2019-03-20 11:12:26 -0600

    Adds migration to add favorites

---
+++ play_backend/db/migrations/20190320104850_initial.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('favorites', function(table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('artist_name');
+      table.string('genre');
+      table.integer('rating');
+
+      table.timestamps(true, true);
+    })
+  ])
+}
+
+exports.down = function(knex, Promise) {
+  knex.schema.dropTable('favorites')
+}
