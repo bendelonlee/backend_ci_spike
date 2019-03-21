6a6661b14897cf5a9adbdeeb64a7493f795181ff
Anten Skrabec <cebarks@gmail.com>
2019-03-21 12:53:04 -0600

    finish off database creation

--- db/migrations/20190320104850_initial.js
+++ db/migrations/20190320104850_initial.js
@@ -9,10 +9,27 @@ exports.up = function(knex, Promise) {
       table.integer('rating');
 
       table.timestamps(true, true);
-    }
-  ])
+    }),
+    knex.schema.createTable('favorites_playlists', function(table){
+      table.increments('id').primary();
+      table.integer('favorite_id').references('favorites.id');
+      table.integer('playlist_id').references('playlists.id');
+
+      table.timestamps(true, true);
+    }),
+    knex.schema.createTable('playlists', function(table){
+      table.increments('id').primary();
+      table.string('title');
+
+      table.timestamps(true, true);
+    })
+  ]);
 }
 
 exports.down = function(knex, Promise) {
-  knex.schema.dropTable('favorites')
+  return Promise.all([
+    knex.schema.dropTable('favorites'),
+    knex.schema.dropTable('favorites_playlists'),
+    knex.schema.dropTable('playlists')
+  ]);
 }
--- knexfile.js
+++ knexfile.js
@@ -28,17 +28,14 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    connection: 'postgres://obyieuwmhdgsrh:253ab3b3a1861f889ca89b4d26637b790199b0b9d784c49aee04b45ba8d03e9b@ec2-184-73-216-48.compute-1.amazonaws.com:5432/d96poj97d1vg7q',
+    useNullAsDefault: true,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './db/migrations'
     }
   }
 };
